c949872e00c968d1539c6ca265180f0b94a58962
Nils Franzen <github@franzens.org>
2014-05-07 11:57:48 +0200

    Added the files

--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@ source 'https://rubygems.org'
 
 ruby "2.0.0"
 
-gem 'eventmachine'
-gem 'thin'
+gem 'eventmachine', '~> 1.0.3'
+gem 'thin', '~> 1.6.2'
---
+++ server.rb
@@ -0,0 +1,41 @@
+require 'rubygems'
+require 'eventmachine'
+require_relative './thin_http_parser.rb'
+ 
+# Freeze some HTTP header names & values
+KEEPALIVE = "Connection: Keep-Alive\r\n".freeze
+ 
+class RequestHandler < EM::Connection
+  def post_init
+    @parser = RequestParser.new
+  end
+ 
+  def receive_data(data)
+    handle_http_request if @parser.parse(data)
+  end
+ 
+  def handle_http_request
+    p [@parser.env, @parser.body.string]
+    keep_alive = @parser.persistent?
+ 
+    data = "OK"
+    send_data("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: #{data.bytesize}\r\n#{ keep_alive  ? KEEPALIVE.clone : nil}\r\n#{data}")
+     
+    if keep_alive
+      post_init
+    else
+      close_connection_after_writing
+    end
+  end
+end
+ 
+host,port = "0.0.0.0", 8083
+puts "Starting server on #{host}:#{port}, #{EM::set_descriptor_table_size(32768)} sockets"
+EM.run do
+  EM.start_server host, port, RequestHandler
+  if ARGV.size > 0
+    forks = ARGV[0].to_i
+    puts "... forking #{forks} times => #{2**forks} instances"
+    forks.times { fork }
+  end
+end
